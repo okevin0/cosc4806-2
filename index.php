44a31490685ce6edb289a4b197ad5e20dbf2f7eb
Kevin Lin <kevin.shuminglin@gmail.com>
2024-05-28 02:01:58 +0000

    clone Assigment1 and update head

--- index.php
+++ index.php
@@ -16,9 +16,9 @@
     <title>Shuming Lin</title>
   </head>
   
-  <body
+  <body>
     
-    <h1>COSC4806 - Assignment1</h1>
+    <h1>COSC4806 - Assignment2</h1>
   
     <p>Welcome, <?php echo $_SESSION['username'] . " and today is " . date("Y-m-d H:i:s"); ?></p>
     
