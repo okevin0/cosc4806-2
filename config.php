414d9f179d058c26a71fe4f0de1702cf8811e4d9
Kevin Lin <kevin.shuminglin@gmail.com>
2024-05-28 02:19:35 +0000

    create config for database connection
    update database information

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+// set database connection info
+defind('DB_HOST', 'n3s.h.filess.io');
+defind('DB_USER', 'cosc4806_smallpick');
+defind('DB_PASS', $ENV['DB_PASS']);
+defind('DB_DATABASE', 'cosc4806_smallpick');
+defind('DB_PORT', '3306');
+
+?>
